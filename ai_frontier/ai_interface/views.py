5b876c15d1248df707f41a10493f62149b1855ed
Bramha-Bajannavar <altbramhabajannavar@gmail.com>
2024-11-17 22:18:51 +0530

    made working link

--- ai_frontier/ai_interface/views.py
+++ ai_frontier/ai_interface/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .DevHub import get_response  # Import the function from the converted script
 
 def index(request):
     return render(request, 'index.html')
 
 def prompt_interface(request):
+    if request.method == 'POST':
+        user_input = request.POST.get('prompt')
+        # Call the get_response function with the user input
+        response = get_response(user_input)
+        return JsonResponse({'response': response})
     return render(request, 'ai_interface/prompt_interface.html')
